4bca8f3e7d2556e1147ddc483510d3c200e725f1
AditiAirboxr <aditi@airboxr.com>
2023-09-20 09:37:36 +1000

    Added virtual display to see headed mode

--- tests/addingHopSam.spec.js
+++ tests/addingHopSam.spec.js
@@ -3,7 +3,7 @@ const login = require('../loginSam');
 const { test, expect, chromium} = require('@playwright/test');
 
 test('Successful Login', async ({ }, testInfo) => {
-  testInfo.setTimeout(90000);
+  testInfo.setTimeout(70000);
   const browser = await chromium.launch({
     headless: false,
     args: ["--disable-blink-features=AutomationControlled", "--disable-popup-blocking"],
